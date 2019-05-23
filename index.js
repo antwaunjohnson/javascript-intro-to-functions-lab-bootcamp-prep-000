cc4419dc869b81b5b18d0f9b6bacca06cf309118
helpful-database-6850 <antwaun.l.johnson@gmail.com>
2019-05-23 21:26:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma (string){
-  if (string === toLowerCase())
- } else if {
-  (string === toUpperCase())
-  
+function sayHiToGrandma(string) {
+	if (string === string.toLowerCase())
+  return "I can\'t hear you!"
+} else if {
+	(string === string.toUpperCase())
+  return "YES INDEED!"
 } else {
- string
+	return "I love you, too"
 }
